cf3a0cfadc36706072fd6698cbc6fabdb3048414
whiteskylark <razilibragimov1990@gmail.com>
2023-01-07 22:38:52 +0300

    Add shell context processor

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config():
   SECRET_KEY = 'Basic g#fbxD2@&l3)sGN%87'
   SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
