286aabe18a1fb09e022fb696df998b14ef209aba
whiteskylark <razilibragimov1990@gmail.com>
2023-01-14 17:12:31 +0300

    Add user profile

--- app/models.py
+++ app/models.py
@@ -1,13 +1,16 @@
 from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from hashlib import md5
+from datetime import datetime
 
 class User(UserMixin, db.Model):
   id = db.Column(db.Integer, index=True, primary_key=True)
   username = db.Column(db.String(14), index=True, unique=True)
   email = db.Column(db.String(120), index=True, unique=True)
   password_hash = db.Column(db.String(128))
+  about_me = db.Column(db.String(140))
+  last_seen = db.Column(db.DateTime, default=datetime)
 
   def __repr__(self):
     return f'<User {self.username}>'
@@ -17,7 +20,13 @@ class User(UserMixin, db.Model):
 
   def check_password(self, password):
     return check_password_hash(self.password_hash, password)
+  
+  def avatar(self, size):
+    digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+    return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 @login.user_loader
 def load_user(id):
   return User.query.get(int(id))
+
+
--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,13 @@ from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, login_required, logout_user
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
+
+@app.before_request
+def before_request():
+  if current_user.is_authenticated:
+    current_user.last_seen = datetime.utcnow()
+    db.session.commit()
 
 @app.route('/')
 @app.route('/index')
@@ -46,3 +53,9 @@ def register():
 def logout():
   logout_user()
   return redirect(url_for('index'))
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+  user = User.query.filter_by(username=username).first_or_404()
+  return render_template('user.html', user=user)
